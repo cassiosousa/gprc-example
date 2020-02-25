3b722f419e7be113e957892ac88c357c7c138702
Cassio Sousa <cassio-sousa@outlook.com>
2020-02-25 14:31:00 -0300

    Initial import

---
+++ server/src/main/java/br/com/gprcexample/GprcExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.gprcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GprcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GprcExampleApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/br/com/gprcexample/GprcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gprcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GprcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
